dc3a10338154270af1f41d803c7ad5d945e0f332
An <an.benjamin@yahoo.com>
2018-10-03 14:56:05 +0800

    origin version

---
+++ app/src/main/java/com/example/benjaminan/test2/OneFragment.java
@@ -0,0 +1,24 @@
+package com.example.benjaminan.test2;
+
+/**
+ * Created by BenjaminAn on 2018/9/26.
+ */
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class OneFragment extends Fragment {
+    public OneFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_one, container, false);
+    }
+}
+
---
+++ app/src/main/java/com/example/benjaminan/test2/ThreeFragment.java
@@ -0,0 +1,23 @@
+package com.example.benjaminan.test2;
+
+/**
+ * Created by BenjaminAn on 2018/9/26.
+ */
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ThreeFragment extends Fragment {
+    public ThreeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_three, container, false);
+    }
+}
